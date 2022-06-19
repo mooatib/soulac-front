4e49c560cb84f1c1d7342a0d5e4464e6fbd6f2ef
mooatib <axel.raux@gmail.com>
2022-06-19 10:01:21 +0200

    2-AuthInterceptor: when 401 is intercepted user is logout and redicted to login page

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -1,22 +1,28 @@
 import {
   HttpErrorResponse,
-  HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { catchError, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { UserService } from '../services/user/user.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private injector: Injector, private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private userService: UserService,
+    private router: Router
+  ) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          // userService.isUserLogged() = false;
+          this.userService.logout();
+          this.router.navigate(['/login']);
           // Should refresh token
         }
         this.snackBar.open(error.error.detail);
